5eb65119d3309a9034d7378b9db8d2b87da0e134
Maxim (neiromaster) Gavrilenko <parrysh@mail.ru>
2026-08-23 10:46:38 +0500

    chore: remove legacy engine, retarget root tooling at workspace

--- vitest.e2e.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  test: {
-    globals: true,
-    include: ['tests/e2e/**/*.test.ts'],
-    testTimeout: 30_000,
-  },
-})
